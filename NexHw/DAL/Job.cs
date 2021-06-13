e8b0aac9027565356829b9d9bbca4659ff33cf91
Mintál Flavián <m.flavian89@gmail.com>
2021-06-13 07:20:21 +0200

    Add basic classes

---
+++ NexHw/DAL/Job.cs
@@ -0,0 +1,21 @@
+﻿using NexHw.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexHw.DAL
+{
+    class Job : IJob
+    {
+        public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Job(int id, string type)
+        {
+            ID = id;
+            Type = type;
+        }        
+    }
+}
---
+++ NexHw/DAL/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using NexHw.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexHw.DAL
+{
+    class Vehicle : IVehicle
+    {
+        public int ID { get; }
+
+        public List<string> JobTypes { get; }
+
+        public Vehicle(int id, List<string> jobTypes)
+        {
+            ID = id;
+            JobTypes = jobTypes;
+        }
+    }
+}
