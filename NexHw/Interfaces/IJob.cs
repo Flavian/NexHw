6a2c77467f96ae9cea4f620c5df0d8fe701f4426
Mintál Flavián <m.flavian89@gmail.com>
2021-06-13 07:27:15 +0200

    remove unused references

--- NexHw/Interfaces/IJob.cs
+++ NexHw/Interfaces/IJob.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NexHw.Interfaces
+﻿namespace NexHw.Interfaces
 {
     interface IJob
     {
